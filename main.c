a7bf2daec2612d7cfcf54bb5a4e56c906b553eed
JEREMYMUTAI <106368802+JEREMYMUTAI@users.noreply.github.com>
2022-07-01 11:16:06 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+simple calculator
+created by JEREMY
+on 1st July, 2022
+C89 compiler
+PMIT License
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tSimple calculator!\n");
+    return 0;
+}
---
+++ mainjerry.c
@@ -0,0 +1,36 @@
+/*
+simple calculator
+created by JEREMY
+on 1st July, 2022
+C89 compiler
+PMIT License
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main
+{
+    int num1,num2, sum, diff,product;
+    float quotient;
+
+    printf("\tSimple calculator!\n");
+    printf ("Enter two numbers :  ");
+    scanf("%d%d",&num1,&num2);
+
+    //computation
+    sum = num1 + num2;
+    diff = num1 + num2;
+    product = num1 * num2;
+    quotient = (floet)num1/num2;
+
+    //output
+    printf ("%d+%d=%d\n",num1,num2,sum);
+    printf ("%d+%d=%d\n",num.num2,diff);
+    printf ("%d+%d=%d\n",num.num2,product);
+    printf ("%d+%d=%d\n",num.num2,quotient);
+
+
+
+    return 0;
+(
